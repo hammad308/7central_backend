4a76ed99c0561c94b05d5471f2157c9d988f3301
Muhammad Hammad Ali <mh6139251@gmail.com>
2026-08-03 13:01:50 +0500

    feat: update environment configuration for development; add health check endpoint

--- src/server.js
+++ src/server.js
@@ -54,6 +54,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use(mongoSanitize());
 
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "7Central Backend is running 🚀"
+    });
+});
+
 // ROUTES
 app.use('/api/user', require('./routes/userRoutes'));
 app.use('/api/project', require('./routes/projectRoutes'));
