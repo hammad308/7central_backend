e850b7242413b06dd27428fe605b258668e473e9
Muhammad Hammad Ali <mh6139251@gmail.com>
2026-08-03 12:46:17 +0500

    feat: update Firebase credentials handling to use environment variable

--- src/utils/notifications/messaging.js
+++ src/utils/notifications/messaging.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require('../../boindo-firebase-adminsdk.json');
+const serviceAccount = require(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
